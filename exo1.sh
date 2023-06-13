9e33781ad65adfc7b6a241de23dabe2b50d97991
Jordan-Argyropoulos <argyropoulosjordan@gmail.com>
2023-06-13 15:28:20 +0200

    Suppression des fichiers

--- exo1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "What's your name?"
-
-#Read input in the default variable
-
-read name
-
-echo "Hello ${name}"
--- exo4.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Hello, $LOGNAME"
-echo "Current date is `date +%D`"
-echo "Current direcotry `pwd`"
-echo "Current hour: `date +%T`"
